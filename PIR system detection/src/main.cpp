415e173e2c1f78a2defeba1fd04721a16c33a8ad
afrajaash178-cmyk <afrajaash178@gmail.com>
2026-05-13 11:58:20 +0530

    create code for the system

--- PIR system detection/src/main.cpp
+++ PIR system detection/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define F_CPU 16000000UL
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+#include <avr/io.h>
+#include <util/delay.h>
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+int main(void)
+{
+    // -----------------------------------
+    // Pin Configuration
+    // -----------------------------------
+    // PB2 -> System LED
+    // PB1 -> PIR Sensor Input
+    // PB0 -> Buzzer
+    // PD7 -> Detection LED
+    // -----------------------------------
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Configure outputs
+    DDRB |= (1 << PB2);   // System LED output
+    DDRB |= (1 << PB0);   // Buzzer output
+    DDRD |= (1 << PD7);   // Detection LED output
+
+    // Configure input
+    DDRB &= ~(1 << PB1);  // PIR input
+
+    while (1)
+    {
+        // System ON indication LED
+        PORTB |= (1 << PB2);
+
+        // Check PIR sensor
+        if (PINB & (1 << PB1))
+        {
+            // Motion detected
+
+            PORTB |= (1 << PB0);   // Buzzer ON
+            PORTD |= (1 << PD7);   // LED ON
+        }
+        else
+        {
+            // No motion detected
+
+            PORTB &= ~(1 << PB0);  // Buzzer OFF
+            PORTD &= ~(1 << PD7);  // LED OFF
+        }
+
+        _delay_ms(100);
+    }
 }
